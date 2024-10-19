cd23f456a563cef50ff73dadee59b7f8f93c4e44
Sharmacraetive <ugaasrooble1@gmail.com>
2024-10-18 21:55:42 -0300

    Animated Navigation Project

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let toggle = document.getElementById('toggle');
+
+let nav = document.getElementById('nav');
+
+
+toggle.addEventListener('click', () => {
+
+    nav.classList.toggle('active');
+    
+
+})
